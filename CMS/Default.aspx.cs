aa8c944b444e64f85e30aef39445b994b6d69f11
fauskanger <thomasfauskanger@gmail.com>
2013-04-23 19:29:53 +0200

    Update ManageDB; init() if unitilized.
    Added /CMS-folder with Default.aspx

---
+++ CMS/Default.aspx.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+using System.Data;
+
+using Gaymer.Classes;
+
+public partial class CMS_Default : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        DataTable dt = Gaymer.Classes.ManageDB.query("SELECT * FROM User");
+
+        //Output.Text = "Det er " + dt.Rows.Count.ToString() + " brukere i databasen.";
+        
+        Output.Text = "Det er brukere i databasen.";
+
+        
+    }
+}
